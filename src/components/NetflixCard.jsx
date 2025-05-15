28c7e711b67a9cf3db4770687a487352c9342fe5
pankaj kumar singh <psingh227003@gmail.com>
2025-05-15 16:26:09 +0530

    little change in netflixcard

--- src/components/NetflixCard.jsx
+++ src/components/NetflixCard.jsx
@@ -4,13 +4,13 @@ export const NetflixCard = ({ curElem }) => {
   return (
     <li>
       <div>
-        <img src={img_url} alt="qot.png" width="40%" height="40%" />
+        <img src={img_url} alt={name} width="40%" height="40%" />
         <h1>Name: {name}</h1>
         <strong>Rating:{rating}</strong>
-        <p>Description={description} </p>
+        <p>Description: {description} </p>
         <p>Caste: {cast}</p>
         <p>Genre: {genre} </p>
-        <a href={watch_url} target="_blank">
+        <a href={watch_url} target="_blank" rel='noopener noreferrer'>
           <button>Watch Now </button>
         </a>
       </div>

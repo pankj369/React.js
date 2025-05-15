6339e867787c3c9b80cd52f135a257103454ec41
pankaj kumar singh <psingh227003@gmail.com>
2025-05-15 22:06:58 +0530

    adding inline css and dynamic css, also template literals  to the netflix card

--- src/components/Netflix.jsx
+++ src/components/Netflix.jsx
@@ -2,11 +2,18 @@ import netflixData from "../api/netflixData.json";
 import { NetflixCard } from "./NetflixCard";
 
 export const Netflix = () => {
+  const listStyle = {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    padding: '2rem',
+  };
+
   return (
-    <ul>
-      {netflixData.map((curElem) =>(
-         <NetflixCard key={curElem.id} curElem={curElem} />
-        ))}
+    <ul style={listStyle}>
+      {netflixData.map((curElem) => (
+        <NetflixCard key={curElem.id} curElem={curElem} />
+      ))}
     </ul>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import './index.css'
+import './index.css'
 import { App} from './App.jsx'
 // import { Profile } from './components/Profile.jsx'
 // impo/rt { Practices } from './practices.jsx'
-import { MovieGalleryApp } from "./components/MovieGalleryApp";
+// import { MovieGalleryApp } from "./components/MovieGalleryApp";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
